ad2ffd3b0d2aa5b11507fac665d492bb80642b5f
OppositeNor <68041679+OppositeNor@users.noreply.github.com>
2023-11-01 15:37:50 -0400

    fix vector bug

--- include/kos_engine/math/vector.hpp
+++ include/kos_engine/math/vector.hpp
@@ -197,14 +197,14 @@ inline float KEDot(const KEVector<N>& vec_1, const KEVector<N>& vec_2) noexcept
 
 inline float KECross(const KEVector2& vec_1, const KEVector2& vec_2) noexcept
 {
-    return vec_1[0] * vec_2[1] - vec_1[1] * vec_2[0];
+    return vec_1[1] * vec_2[0] - vec_1[0] * vec_2[1];
 }
 
 inline KEVector<3> KECross(const KEVector<3>& vec_1, const KEVector<3>& vec_2) noexcept
 {
     return KEVector<3>({
-        vec_1[1] * vec_2[2] - vec_1[2] * vec_2[1],
-        vec_1[2] * vec_2[0] - vec_1[0] * vec_2[2],
-        vec_1[0] * vec_2[1] - vec_1[1] * vec_2[0]
+        vec_1[2] * vec_2[1] - vec_1[1] * vec_2[2],
+        vec_1[0] * vec_2[2] - vec_1[2] * vec_2[0],
+        vec_1[1] * vec_2[0] - vec_1[0] * vec_2[1]
     });
 }
